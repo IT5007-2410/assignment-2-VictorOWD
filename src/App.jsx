a371279eddc2da690448d0da04bc7c1acf45feb9
VictorOWD <E1325163@u.nus.edu>
2024-10-01 03:24:21 +0800

    added bootstrap styling | added active traveller count in each page

--- src/App.jsx
+++ src/App.jsx
@@ -40,72 +40,83 @@ const initialTravellers = {
 */
 
 function TravellerRow({ id, activeTraveller, deleteFunc }) {
-  {
-    /*Q3. Placeholder to initialize local variable based on traveller prop.*/
-  }
   return (
     <tr>
       <td>{id}</td>
-      {/*Q3. Placeholder for rendering one row of a table with required traveller attribute values.*/}
       {Object.entries(activeTraveller).map(([key, value], index) => {
         if (key == "isDisplayed") return null;
         return <td key={index}>{value}</td>;
       })}
       <td>
-        <button onClick={() => deleteFunc(id)}>Delete</button>
+        <button class="btn btn-danger" onClick={() => deleteFunc(id)}>
+          Delete
+        </button>
       </td>
     </tr>
   );
 }
 
-function DisplayPage({ activeTravellers, deleteFunc }) {
-  /*Q3. Write code to render rows of table, reach corresponding to one traveller. Make use of the TravellerRow function that draws one row.*/
+function ListPage({ activeTravellers, deleteFunc }) {
+  const numberOfTravellers = Object.keys(activeTravellers).length;
+
   return (
     <div>
-      <h2>Detailed View</h2>
-      <table>
-        <thead>
-          <tr>
-            {/*Q3. Below table is just an example. Add more columns based on the traveller attributes you choose.*/}
-            <th>ID</th>
-            <th>Salutation</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Date Of Birth</th>
-            <th>Passport No.</th>
-            <th>Nationality</th>
-            <th>Country Of Residence</th>
-            <th>Phone</th>
-            <th>Email</th>
-            <th>Booking Time</th>
-            <th>Amend</th>
-          </tr>
-        </thead>
-        <tbody>
-          {/*Q3. write code to call the JS variable defined at the top of this function to render table rows.*/}
-          {Object.keys(activeTravellers).length === 0 ? (
-            <tr>
-              <td colSpan={12}>No travellers found. Add a traveller first.</td>
-            </tr>
-          ) : (
-            Object.entries(activeTravellers).map(([id, activeTraveller]) => {
-              return (
-                <TravellerRow
-                  key={id}
-                  id={id}
-                  activeTraveller={activeTraveller}
-                  deleteFunc={deleteFunc}
-                />
-              );
-            })
-          )}
-        </tbody>
-      </table>
+      <div className="container-fluid">
+        <div>
+          <h3 className="text-center">Passenger List</h3>
+          <p className="text-center fw-semibold">
+            {numberOfTravellers} passengers onboarded
+          </p>
+        </div>
+        <div className="table-responsive">
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Salutation</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Date Of Birth</th>
+                <th>Passport No.</th>
+                <th>Nationality</th>
+                <th>Country Of Residence</th>
+                <th>Phone</th>
+                <th>Email</th>
+                <th>Booking Time</th>
+                <th>Amend</th>
+              </tr>
+            </thead>
+            <tbody className="table-group-divider">
+              {Object.keys(activeTravellers).length === 0 ? (
+                <tr>
+                  <td colSpan={12}>
+                    No travellers found. Add a traveller first.
+                  </td>
+                </tr>
+              ) : (
+                Object.entries(activeTravellers).map(
+                  ([id, activeTraveller]) => {
+                    return (
+                      <TravellerRow
+                        key={id}
+                        id={id}
+                        activeTraveller={activeTraveller}
+                        deleteFunc={deleteFunc}
+                      />
+                    );
+                  }
+                )
+              )}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
 
-function AddPage({ currentId, addFunc }) {
+function AddPage({ activeTravellers, currentId, addFunc }) {
+  const numberOfTravellers = Object.keys(activeTravellers).length;
   const emptyFormFields = {
     salutation: "",
     firstName: "",
@@ -125,7 +136,6 @@ function AddPage({ currentId, addFunc }) {
   const [success, setSuccess] = React.useState(true);
 
   function handleSubmit(e) {
-    /*Q4. Fetch the passenger details from the add form and call bookTraveller()*/
     e.preventDefault();
     if (addFunc(traveller)) {
       setMessage("Successfully added traveller!");
@@ -148,124 +158,135 @@ function AddPage({ currentId, addFunc }) {
 
   return (
     <div>
-      <h2>Add a Traveller</h2>
-      <form name="addTraveller" onSubmit={handleSubmit}>
-        {/*Q4. Placeholder to enter passenger details. Below code is just an example.*/}
-        <FormField
-          label="ID"
-          type="text"
-          name="id"
-          id="id"
-          value={currentId}
-          placeholder={null}
-          onChange={null}
-          disabled={true}
-        />
-        <FormField
-          label="Salutation"
-          type="text"
-          name="salutation"
-          id="salutation"
-          value={traveller.salutation}
-          placeholder="Mr, Master, Mdm, Ms"
-          onChange={handleChange}
-          disabled={false}
-        />
-        <FormField
-          label="First Name"
-          type="text"
-          name="firstName"
-          id="first-name"
-          value={traveller.firstName}
-          placeholder="John"
-          onChange={handleChange}
-          disabled={false}
-        />
-        <FormField
-          label="Last Name"
-          type="text"
-          name="lastName"
-          id="last-name"
-          value={traveller.lastName}
-          placeholder="Doe"
-          onChange={handleChange}
-          disabled={false}
-        />
-        <FormField
-          label="Date of Birth"
-          type="date"
-          name="dob"
-          id="dob"
-          value={traveller.dob}
-          placeholder="1970-1-1"
-          onChange={handleChange}
-          disabled={false}
-        />
-        <FormField
-          label="Passport Number"
-          type="text"
-          name="passportNumber"
-          id="pasport-number"
-          value={traveller.passportNumber}
-          placeholder="E1234567X"
-          onChange={handleChange}
-          disabled={false}
-        />
-        <FormField
-          label="Nationality"
-          type="text"
-          name="nationality"
-          id="nationality"
-          value={traveller.nationality}
-          placeholder="Singaporean"
-          onChange={handleChange}
-          disabled={false}
-        />
-        <FormField
-          label="Country of Residence"
-          type="text"
-          name="countryOfResidence"
-          id="country-of-residence"
-          value={traveller.countryOfResidence}
-          placeholder="Singapore"
-          onChange={handleChange}
-          disabled={false}
-        />
-        <FormField
-          label="Phone Number"
-          type="number"
-          name="phoneNumber"
-          id="phone-number"
-          value={traveller.phoneNumber}
-          placeholder="98765432"
-          onChange={handleChange}
-          disabled={false}
-        />
-        <FormField
-          label="Email"
-          type="email"
-          name="email"
-          id="email"
-          value={traveller.email}
-          placeholder="hello@tickettoride.com"
-          onChange={handleChange}
-          disabled={false}
-        />
-        <FormField
-          label="Booking Time"
-          type="datetime-local"
-          name="bookingTime"
-          id="booking-time"
-          value={traveller.bookingTime}
-          placeholder="2024-09-30T03:00"
-          onChange={handleChange}
-          disabled={false}
-        />
-        <button type="submit">Add Traveller</button>
-      </form>
-      {message && (
-        <div style={{ color: success ? "green" : "red" }}>{message}</div>
-      )}
+      <div className="container-fluid">
+        <div>
+          <h3 className="text-center">Add a Traveller</h3>
+          <p className="text-center fw-semibold">
+            {maxSeats - numberOfTravellers} seats remaining
+          </p>
+        </div>
+        <form name="addTraveller" onSubmit={handleSubmit}>
+          {/*Q4. Placeholder to enter passenger details. Below code is just an example.*/}
+          <FormField
+            label="ID"
+            type="text"
+            name="id"
+            id="id"
+            value={currentId}
+            placeholder={null}
+            onChange={null}
+            disabled={true}
+          />
+          <FormField
+            label="Salutation"
+            type="text"
+            name="salutation"
+            id="salutation"
+            value={traveller.salutation}
+            placeholder="Mr, Master, Mdm, Ms"
+            onChange={handleChange}
+            disabled={false}
+          />
+          <FormField
+            label="First Name"
+            type="text"
+            name="firstName"
+            id="first-name"
+            value={traveller.firstName}
+            placeholder="John"
+            onChange={handleChange}
+            disabled={false}
+          />
+          <FormField
+            label="Last Name"
+            type="text"
+            name="lastName"
+            id="last-name"
+            value={traveller.lastName}
+            placeholder="Doe"
+            onChange={handleChange}
+            disabled={false}
+          />
+          <FormField
+            label="Date of Birth"
+            type="date"
+            name="dob"
+            id="dob"
+            value={traveller.dob}
+            placeholder="1970-1-1"
+            onChange={handleChange}
+            disabled={false}
+          />
+          <FormField
+            label="Passport Number"
+            type="text"
+            name="passportNumber"
+            id="pasport-number"
+            value={traveller.passportNumber}
+            placeholder="E1234567X"
+            onChange={handleChange}
+            disabled={false}
+          />
+          <FormField
+            label="Nationality"
+            type="text"
+            name="nationality"
+            id="nationality"
+            value={traveller.nationality}
+            placeholder="Singaporean"
+            onChange={handleChange}
+            disabled={false}
+          />
+          <FormField
+            label="Country of Residence"
+            type="text"
+            name="countryOfResidence"
+            id="country-of-residence"
+            value={traveller.countryOfResidence}
+            placeholder="Singapore"
+            onChange={handleChange}
+            disabled={false}
+          />
+          <FormField
+            label="Phone Number"
+            type="number"
+            name="phoneNumber"
+            id="phone-number"
+            value={traveller.phoneNumber}
+            placeholder="98765432"
+            onChange={handleChange}
+            disabled={false}
+          />
+          <FormField
+            label="Email"
+            type="email"
+            name="email"
+            id="email"
+            value={traveller.email}
+            placeholder="hello@tickettoride.com"
+            onChange={handleChange}
+            disabled={false}
+          />
+          <FormField
+            label="Booking Time"
+            type="datetime-local"
+            name="bookingTime"
+            id="booking-time"
+            value={traveller.bookingTime}
+            placeholder="2024-09-30T03:00"
+            onChange={handleChange}
+            disabled={false}
+          />
+          <div className="d-flex flex-column align-items-center mt-3">
+            <button className="btn btn-outline-primary" type="submit">
+              Add Traveller
+            </button>
+          </div>
+        </form>
+        {message && (
+          <div style={{ color: success ? "green" : "red" }}>{message}</div>
+        )}
+      </div>
     </div>
   );
 }
@@ -281,10 +302,12 @@ function FormField({
   disabled,
 }) {
   return (
-    <div>
-      <label htmlFor={id}>{label}</label>
-      <br />
+    <div className="d-flex flex-column justify-content-center align-items-center">
+      <label className="form-label w-50 text-start" htmlFor={id}>
+        {label}
+      </label>
       <input
+        className="form-control form-control-sm w-50"
         id={id}
         type={type}
         name={name}
@@ -319,28 +342,28 @@ function HomePage({ activeTravellers }) {
   }
 
   return (
-    <div style={{ display: "flex", flexFlow: "column wrap", gap: "10px" }}>
-      {/*Q2. Placeholder for Homepage code that shows free seats visually.*/}
-      <h2>Overview</h2>
-      <div style={{ display: "flex", flexFlow: "column wrap" }}>
-        <h3>Legend</h3>
-        <div style={{ display: "flex", flexFlow: "row wrap", gap: "10px" }}>
-          <SeatBox isOccupied={true} text="occupied" />
-          <SeatBox isOccupied={false} text="Available" />
-        </div>
+    <div>
+      <div className="container-fluid">
+        <h3 className="text-center">Seating Capacity</h3>
+        <p className="text-center fw-semibold">
+          {maxSeats - occupiedSeats} / {maxSeats} seats remaining
+        </p>
       </div>
-      <hr style={{ width: "100%", height: "3px" }} />
-      <div style={{ display: "flex", flexFlow: "column wrap" }}>
-        <h3>Current Capacity</h3>
-        <div
-          style={{
-            display: "flex",
-            flexFlow: "row wrap",
-            gap: "10px",
-            width: "230px",
-          }}
-        >
-          {renderSeats()}
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col border-end d-flex flex-column">
+            <h4 className="text-center">Legend</h4>
+            <div className="d-flex flex-row justify-content-center gap-2">
+              <SeatBox isOccupied={true} text="Occupied" />
+              <SeatBox isOccupied={false} text="Available" />
+            </div>
+          </div>
+          <div className="col border-start d-flex flex-column">
+            <h4 className="text-center">Current Capacity</h4>
+            <div className="d-flex flex-row flex-wrap justify-content-center gap-2">
+              {renderSeats()}
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -351,14 +374,11 @@ function SeatBox({ isOccupied, text }) {
   const seatColor = isOccupied ? "red" : "green";
   return (
     <div
+      className="rounded-top-circle d-flex justify-content-center align-items-center border border-2 border-dark fw-bold"
       style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: seatColor,
         width: "100px",
         height: "100px",
-        wordWrap: "break-word",
+        backgroundColor: seatColor,
       }}
     >
       {text}
@@ -381,7 +401,6 @@ class TicketToRide extends React.Component {
   }
 
   setSelector(value) {
-    /*Q2. Function to set the value of component selector variable based on user's button click.*/
     this.setState({ selector: value });
   }
 
@@ -429,7 +448,6 @@ class TicketToRide extends React.Component {
   }
 
   bookTraveller(newTraveller) {
-    /*Q4. Write code to add a passenger to the traveller state variable.*/
     if (Object.keys(this.state.activeTravellers).length == maxSeats) {
       return false;
     }
@@ -457,7 +475,6 @@ class TicketToRide extends React.Component {
   }
 
   deleteTraveller(travellerKey) {
-    /*Q5. Write code to delete a passenger from the traveller state variable.*/
     if (window.confirm(`Confirm to delete traveller id: ${travellerKey}`)) {
       const allTravellers = JSON.parse(localStorage.getItem("allTravellers"));
 
@@ -474,26 +491,20 @@ class TicketToRide extends React.Component {
   render() {
     return (
       <div>
-        <h1>Ticket To Ride</h1>
-        <div>
-          {/*Q2. Code for Navigation bar. Use basic buttons to create a nav bar. Use states to manage selection.*/}
+        <header>
           <Nav navFunc={this.setSelector} />
-        </div>
-        {/*Only one of the below four divisions is rendered based on the button clicked by the user.*/}
+        </header>
         <main>
-          {/*Q2 and Q6. Code to call Instance that draws Homepage. Homepage shows Visual Representation of free seats.*/}
-          {/*Q3. Code to call component that Displays Travellers.*/}
-          {/*Q4. Code to call the component that adds a traveller.*/}
-          {/*Q5. Code to call the component that deletes a traveller based on a given attribute.*/}
           {this.state.selector === "Home" ? (
             <HomePage activeTravellers={this.state.activeTravellers} />
-          ) : this.state.selector === "Display" ? (
-            <DisplayPage
+          ) : this.state.selector === "List" ? (
+            <ListPage
               activeTravellers={this.state.activeTravellers}
               deleteFunc={this.deleteTraveller}
             />
           ) : this.state.selector === "Add" ? (
             <AddPage
+              activeTravellers={this.state.activeTravellers}
               currentId={this.state.currentId}
               addFunc={this.bookTraveller}
             />
@@ -508,16 +519,47 @@ class TicketToRide extends React.Component {
 
 function Nav({ navFunc }) {
   return (
-    <nav>
-      <NavLink text="Home" clickFn={navFunc} selector="Home" />
-      <NavLink text="Display" clickFn={navFunc} selector="Display" />
-      <NavLink text="Add" clickFn={navFunc} selector="Add" />
+    <nav className="navbar navbar-expand-sm">
+      <div className="container-fluid">
+        <span className="navbar-brand">Ticket To Ride</span>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarNav"
+        >
+          <div className="navbar-nav">
+            <NavLink
+              text="Seating Capacity"
+              clickFn={navFunc}
+              selector="Home"
+            />
+            <NavLink text="Passenger List" clickFn={navFunc} selector="List" />
+            <NavLink text="Add Passenger" clickFn={navFunc} selector="Add" />
+          </div>
+        </div>
+      </div>
     </nav>
   );
 }
 
 function NavLink({ text, clickFn, selector }) {
-  return <button onClick={() => clickFn(selector)}>{text}</button>;
+  return (
+    <div className="nav-item">
+      <button className="nav-link" onClick={() => clickFn(selector)}>
+        {text}
+      </button>
+    </div>
+  );
 }
 
 const element = <TicketToRide />;
